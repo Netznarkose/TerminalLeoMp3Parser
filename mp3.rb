ce8c3bf0cb87ca97ce5bab85b7ddd9f53b0c510b
Netznarkose <mmaintz@posteo.de>
2015-02-03 19:53:39 +0100

    refactoring 4

--- mp3.rb
+++ mp3.rb
@@ -5,6 +5,9 @@ require 'open-uri'
 require 'Clipboard'
 require 'colorize'
 
+
+# Methods
+
 def ansage_1 data
   puts
   puts  'Leo-Mp3 Finder'
@@ -33,9 +36,24 @@ def select_word
 	puts ""
   @begriff = gets.chomp!
 end
+def process_url
+  @begriff.gsub!(' ', '%20') # Leerzeichen in der Eingabe werden mit "%20" ausgetauscht weil Leo das so möchte!  
+  url_en = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"             
+  url_es = "http://dict.leo.org/dictQuery/m-vocab/esde/query.xml?tolerMode=nof&lp=esde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
+  url_fr = "http://dict.leo.org/dictQuery/m-vocab/frde/query.xml?tolerMode=nof&lp=frde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
+  case
+	when @sprache == "e"
+	  @url = url_en
+	when @sprache == "s"
+	  @url = url_es
+	when @sprache == "f"
+	  @url = url_fr
+	end	
+end
+
+# let the app beginn
 
 array = []
-
 loop do
 ansage_1 array
 select_language
@@ -43,28 +61,13 @@ break if @sprache == "x"
 select_word
 array.push @begriff
 break if @begriff == "x"
-@begriff.gsub!(' ', '%20') # Leerzeichen in der Eingabe werden mit "%20" ausgetauscht weil Leo das so möchte!  
-url_en = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"             
-url_es = "http://dict.leo.org/dictQuery/m-vocab/esde/query.xml?tolerMode=nof&lp=esde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
-            
-url_fr = "http://dict.leo.org/dictQuery/m-vocab/frde/query.xml?tolerMode=nof&lp=frde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
+process_url 
 
-	case
-	when @sprache == "e"
-	url = url_en
-	when @sprache == "s"
-	url = url_es
-	when @sprache == "f"
-	url = url_fr
-	end	
-	
-	data = Nokogiri::XML(open(url)) # Variable aus dem case-test
+	data = Nokogiri::XML(open(@url)) # Variable aus dem case-test
 	variable = data.at_css('pron').first[1]
-	$ergebnis = "http://dict.leo.org/media/audio/#{variable}.mp3" #$ergebnis = globale Variable
+	Clipboard.copy "http://dict.leo.org/media/audio/#{variable}.mp3"
 	puts
-	ansage =  "Mp3 was copied to the Clipboard"
-	puts ansage.red
-	Clipboard.copy $ergebnis 
+	puts "Mp3 was copied to the Clipboard".red
 	puts
 	puts
   `sleep 1` # puts bash for one second two sleep
