983a2c93772229698d0f2b7a0768172910b76892
Netznarkose <mmaintz@posteo.de>
2015-02-03 18:30:16 +0100

    refactoring 2

--- mp3.rb
+++ mp3.rb
@@ -5,41 +5,45 @@ require 'open-uri'
 require 'Clipboard'
 require 'colorize'
 
+def ansage_1 data
+  puts
+  puts  'Leo-Mp3 Finder'
+  puts  '----------------------------------'
+  puts
+  puts  'Bisherige Vokabeln'
+  puts
+  puts data
+  puts
+  puts
+  puts  'Sprachen'
+  puts  "----------------------------------"
+  puts  'english = e'
+  puts  'spanish = s'
+  puts  'french = f'
+  puts
+  puts  'exit = x'
+  puts
+end
+def ansage_2 
+  puts
+	puts 'Which word/ expression? => exit = x'
+	puts ""
+end
+
 array = []
 
 loop do
-
-puts
-puts  'Leo-Mp3 Finder'
-puts  '----------------------------------'
-puts
-puts  'Bisherige Vokabeln'
-puts
-puts array
-puts
-puts
-puts  'Sprachen'
-puts  "----------------------------------"
-puts  'english = e'
-puts  'spanish = s'
-puts  'french = f'
-puts
-puts  'exit = x'
-puts
+ansage_1 array
 sprache = gets.chomp!
 break if sprache == "x"
-	puts
-	puts 'Which word/ expression? => exit = x'
-	puts ""
-	begriff = gets.chomp!
-	array.push "#{begriff}"
+ansage_2
+begriff = gets.chomp!
+array.push begriff
 break if begriff == "x"
-		
-        begriff.gsub!(' ', '%20') # Leerzeichen in der Eingabe werden mit "%20" ausgetauscht weil Leo das so möchte!	
-
-	url_en = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search=#{begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"        
-        
-        url_es = "http://dict.leo.org/dictQuery/m-vocab/esde/query.xml?tolerMode=nof&lp=esde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16" 
+begriff.gsub!(' ', '%20') # Leerzeichen in der Eingabe werden mit "%20" ausgetauscht weil Leo das so möchte!	
+file = open("file_url_en", "r"); url_en = file.read
+puts url_en 
+url_es = "http://dict.leo.org/dictQuery/m-vocab/esde/query.xml?tolerMode=nof&lp=esde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16" 
 	
 
 	url_fr = "http://dict.leo.org/dictQuery/m-vocab/frde/query.xml?tolerMode=nof&lp=frde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
