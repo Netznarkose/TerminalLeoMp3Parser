123a77b14c362df472307c4e85dd2f7a14307da6
Netznarkose <mmaintz@posteo.de>
2015-02-04 17:57:03 +0100

    refactoring 9

--- mp3.rb
+++ mp3.rb
@@ -12,16 +12,17 @@ def default_settings
   @sprache = "e"
 end
 def lenguage
-@hash = { "e" => "english", "s" => "spanish", "f" => "french" }
-puts  "lenguage is set to: #{@hash[@sprache].red}"
-end
-def register
-@array = []
-@array = "no entrys yet".red if @array.empty?
-puts  "Bisherige Vokabeln: #{@array}"
+  @hash = { "e" => "english", "s" => "spanish", "f" => "french" }
+  puts  "lenguage is set to: #{@hash[@sprache].red}"
 end
 
-def warm_welcome data
+def register
+  @array = []
+  @array = "no entrys yet".red if @array.empty?
+  puts  "Bisherige Vokabeln: #{@array}"
+end
+
+def warm_welcome 
   puts  '##################################'.green
   puts  '#                                #'.green
   puts  '#                                #'.green
@@ -36,7 +37,9 @@ def warm_welcome data
   `sleep 1` # puts bash for one second two sleep
   puts `clear`# clears bash
 end
-def main_menue(data, sparta)
+
+def main_menue
+puts `clear`# clears bash
 puts  '   ######  Leo-Mp3 Finder #######  Main Meue #######'
 puts
 puts  "Press: 1 for: Welcome 2 for: Changing Languages 3 for:  Searching a Word 4 for: Exiting"
@@ -48,21 +51,31 @@ puts
 return @choice # wahrscheinlich redundant
 end
 def select_language 
-  begin
-  puts `clear`# clears bash
-  puts  '   ######  Leo-Mp3 Finder #######  Select Language  #######'
-  puts  
-  puts  "Press: e for: English s for: Spanish f for: French 4 for: Exiting"
-  puts
-  @sprache = gets.chomp!
-  puts `clear`# clears bash
-  end until @sprache == "e" || @sprache == "s" || @sprache == "f"
+begin
+puts `clear`# clears bash
+puts  '   ######  Leo-Mp3 Finder #######  Select Language  #######'
+puts  
+puts  "Press: e for: English s for: Spanish f for: French 4 for: Exiting"
+puts
+lenguage
+register
+puts
+@sprache = gets.chomp!
+puts `clear`# clears bash
+end until @sprache == "e" || @sprache == "s" || @sprache == "f"
 end
-def select_word data
-  puts  '    ######  Leo-Mp3 Finder #######  Search a Word #######'
-  puts
-	puts 
-  print "word your are looking for:   "; @begriff = gets.chomp!
+def select_word 
+puts `clear`# clears bash
+puts  '    ######  Leo-Mp3 Finder #######  Search a Word #######'
+puts 
+puts "Which Word Are You Looking For ? #### Press => m for Main Menue #####"
+puts
+lenguage
+register
+puts
+puts
+print "word =>  "; @begriff = gets.chomp!
+main_menue if @begriff == "m"
 @begriff.gsub!(' ', '%20') # Leerzeichen in der Eingabe werden mit "%20" ausgetauscht weil Leo das so möchte!  
   url_en = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"             
   url_es = "http://dict.leo.org/dictQuery/m-vocab/esde/query.xml?tolerMode=nof&lp=esde&lang=de&rmWords=off&rmSearch=on&directN=0&search=#{@begriff}&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
@@ -75,7 +88,7 @@ def select_word data
 	when @sprache == "f"
 	  @url = url_fr
 	end	
-#  data.push @begriff
+  @array.push @begriff
 	noko = Nokogiri::XML(open(@url)) # Variable aus dem case-test
 	variable = noko.at_css('pron').first[1]
 	Clipboard.copy "http://dict.leo.org/media/audio/#{variable}.mp3"
@@ -88,38 +101,21 @@ def select_word data
 end
 
 # let the app beginn
-array = []
 default_settings
-warm_welcome array
-main_menue(hash, array)
+warm_welcome 
+main_menue
 loop do
   case @choice 
   when 1
     warm_welcome array
-    main_menue(hash, array)
+    main_menue
   when 2
     select_language 
-    main_menue(hash, array)
+    main_menue
   when 3
-    select_word array
+    select_word 
   when 4
     break
   end
 end
 
-#array.push @begriff
-#break if @begriff == "x"
-#process_url 
-#
-#	data = Nokogiri::XML(open(@url)) # Variable aus dem case-test
-#	variable = data.at_css('pron').first[1]
-#	Clipboard.copy "http://dict.leo.org/media/audio/#{variable}.mp3"
-#	puts
-#	puts "Mp3 was copied to the Clipboard".red
-#	puts
-#	puts
-#  `sleep 1` # puts bash for one second two sleep
-#  puts `clear`# clears bash
-#end
-#
-#
