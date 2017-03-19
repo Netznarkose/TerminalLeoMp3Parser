c0342d7f0de5533d06b35e04369a3a1d1771890e
netznarkose <mmaintz@posteo.de>
2017-03-19 22:01:53 +0100

    refactor prompting_menue

--- process_url.rb
+++ process_url.rb
@@ -8,23 +8,32 @@ class SearchWord
   attr_accessor :language, :term, :url
   def initialize(input_hash = {})
     @term = input_hash[:term] || 'hello'
-    @language = input_hash[:language] || 'e' 
+    @language = input_hash[:language] || 'e'
     @array = []
     @url = ""
   end
-  
-  def prompting_menue
-      puts `clear`# clears bash
-      "######     L E O -- M P 3 -- P A R S E R        ###########         Which Word Are You Looking For         ###### 
-      \n\n###  Press: e => English  s => Spanish  f => French  clear => deleting vocablist  help => Help  exit => Exit ###"
+
+  def prompting_menue # header
+    puts `clear`
+    puts '#' * 6 << ' ' * 6 << 'L E O -- M P 3 -- P A R S E R' << ' ' * 6 << '#' * 6
+    print "\nType:  "; language_hash.map {|key, value| print "#{key} => #{value}   " }
   end
 
-  def prompting_language_display
+  def language_hash
+    {
+      "e" => "English",
+      "s" => "Spanish",
+      "f" => "French",
+      "exit" => "Exit"
+    }
+  end
+
+  def prompting_language_display # language menu
     hash = { "e" => "English", "s" => "Spanish", "f" => "French" }
     "\nLanguage is set to: #{ hash[@language] }"
   end
-  
-  def prompting_register
+
+  def prompting_register # vocab-list
     if @array.empty?
       "\nLooked up Vocabulary: No Activity yet"
     else
@@ -34,42 +43,20 @@ class SearchWord
       "\nLooked up Vocabulary: #{@array}"
     end
   end
-  
+
   def deleting_register
     @array.clear
   end
 
-  def user_input 
+  def user_input
     print "\nword =>  "; @user_input = gets.chomp!
   end
-  
-  def get_mp3 
-    generate_url
-    begin
-      noko = Nokogiri::XML(open(@url)) # Variable aus dem case-test
-      final_url = noko.at_css('pron').first[1]
-    rescue
-      @array.push "####{@term}"
-      flash_message(:danger)
-    else
-      @array.push @term
-      Clipboard.copy "http://dict.leo.org/media/audio/#{final_url}.mp3"
-      flash_message(:success)
-    end
-  end
-  
-private
 
-  def flash_message(data) 
+  private
+
+  def flash_message(data)
     flash = {success: "\nMp3 was copied to the Clipboard", danger: "could not be found"}
     flash[data]
-  end
-  def generate_url
-    select_language = { 'e' => 'ende', 's' => 'esde', 'f' => 'frde' }
-    @url = "http://dict.leo.org/dictQuery/m-vocab/" << select_language[@language]
-    @url << "/query.xml?tolerMode=nof&lp=ende&lang=en&rmWords=off&rmSearch=on&directN=0&search=" 
-    @url << @term 
-    @url << "&searchLoc=0&resultOrder=basic&multiwordShowSingle=on&sectLenMax=16"
   end
 end
 
